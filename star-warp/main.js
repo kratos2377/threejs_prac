57108e6e962e5ef5754e0911207cbe8b064aa1e5
SHOBHIT YADAV <shobityadav23@gmail.com>
2021-12-13 22:20:20 -0800

    Star Warp Fixed

--- star-warp/main.js
+++ star-warp/main.js
@@ -1,5 +1,7 @@
+import * as THREE from "../build/three.module.js";
+
 var scene, camera, renderer;
-console.log("working");
+console.log("working s2s2");
 let LINE_COUNT = 8000;
 let geom = new THREE.BufferGeometry();
 geom.setAttribute(
